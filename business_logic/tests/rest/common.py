2c228ed3fe490a0de661a535669e792a2d4bb327
AZolotukhin <zolotukhin89@gmail.com>
2015-12-28 16:13:20 +0300

    rest: program type list

--- business_logic/tests/rest/common.py
+++ business_logic/tests/rest/common.py
@@ -1,5 +1,34 @@
 # -*- coding: utf-8 -*-
 
+import json
 from django.core.urlresolvers import reverse
 
+from django.test import Client
+
 from ..common import *
+
+
+class JSONClient(Client):
+    def post(self, path, data={}, content_type='application/json',
+             follow=False, **extra):
+        ret = super(Client, self).post(path, data=data,
+            content_type=content_type, follow=follow,
+            HTTP_X_REQUESTED_WITH='XMLHttpRequest',
+            **extra)
+        return ret
+
+    def put(self, path, data={}, content_type='application/json',
+            follow=False, **extra):
+        ret = super(Client, self).put(path, data=data,
+            content_type=content_type, follow=follow,
+            HTTP_X_REQUESTED_WITH='XMLHttpRequest',
+            **extra)
+        return ret
+
+    def delete(self, path, data={}, content_type='application/json',
+            follow=False, **extra):
+        ret = super(Client, self).delete(path, data=data,
+            content_type=content_type, follow=follow,
+            HTTP_X_REQUESTED_WITH='XMLHttpRequest',
+            **extra)
+        return ret
--- business_logic/tests/rest/program_type.py
+++ business_logic/tests/rest/program_type.py
@@ -5,11 +5,19 @@ from .common import *
 
 class ProgramTypeTest(TestCase):
     def setUp(self):
-        self.program_type = ProgramType.objects.create()
+        self.program_type = ProgramType.objects.create(name='test')
+        self.argument = ProgramArgument.objects.create(
+            program_type=self.program_type,
+            content_type=ContentType.objects.get_for_model(TestModel)
+        )
 
-
-    def test_urls(self):
-        url = reverse('business-logic:rest:root')
+        self.client = JSONClient()
 
     def test_program_type_list(self):
-        pass
+        url = reverse('business-logic:rest:program-type-list')
+        response =  self.client.get(url)
+        self.assertEqual(200, response.status_code)
+        _json = json.loads(response.content)
+        self.assertIsInstance(_json, list)
+        self.assertEqual(1, len(_json))
+
