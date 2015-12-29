b604c1cf1ff2d5c2e0a6d7be6be3e9f974d6effd
AZolotukhin <az@cmios.ru>
2015-12-29 12:56:23 +0300

    rest/program-type: schema - verbose_name

--- business_logic/tests/test_app/models.py
+++ business_logic/tests/test_app/models.py
@@ -6,8 +6,8 @@ from django.db import models
 
 
 class TestModel(models.Model):
-    int_value = models.PositiveIntegerField('value', default=0)
-    string_value = models.CharField('string value', max_length=255)
+    int_value = models.PositiveIntegerField('Integer value', default=0)
+    string_value = models.CharField(max_length=255)
     foreign_value = models.ForeignKey('TestRelatedModel', null=True)
 
 
