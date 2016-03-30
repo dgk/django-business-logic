3e3147494ef1c7194360dcde1acc7d467e6f05f0
dgk <dgk@dgk.su>
2016-03-30 12:25:50 +0300

    tree_to_blockly_xml

--- business_logic/tests/blockly.py
+++ business_logic/tests/blockly.py
@@ -4,21 +4,13 @@ from .common import *
 
 from lxml import etree
 
-class NodeXMLTest(TestCase):
-    def test(self):
+
+class TreeToBlocklyXmlTest(TestCase):
+    # https://blockly-demo.appspot.com/static/demos/code/index.html
+    def test_assignment(self):
         entry_point = var_A_assign_1()
         assign_node = entry_point.get_children()[1]
-        xml = tree_to_blockly_xml(assign_node)
 
-        print(etree.tostring(xml, pretty_print=True))
-        class Visitor:
-            def __init__(self):
-                self.visited = []
-
-            def visit(self, visited):
-                self.visited.append(visited.content_object)
-
-        visitor = Visitor()
-
-        entry_point.traverse(visitor)
-        print visitor.visited
+        xml_str = tree_to_blockly_xml(assign_node)
+        xml = etree.fromstring(xml_str)
+        #print(etree.tostring(xml, pretty_print=True))
