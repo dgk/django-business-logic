81829319c18cd52c500ee681c67614465385b476
Dmitry Kuksinsky <dgk@dgk.su>
2016-03-30 12:25:50 +0300

    tree_to_blockly_xml

--- business_logic/blockly.py
+++ business_logic/blockly.py
@@ -1,8 +1,24 @@
 # -*- coding: utf-8 -*-
 
+from .models import NodeVisitor
+
+
+class XmlCreateVisitor(NodeVisitor):
+    def __init__(self):
+        self.clone = None
+
+    def visit(self, node):
+        print node, 'visit'
+
 
 def tree_to_blockly_xml(tree_root):
-    pass
+    visitor = XmlCreateVisitor()
+    tree_root.traverse(visitor)
+
+    return '''<xml xmlns="http://www.w3.org/1999/xhtml">
+        <block></block>
+        </xml>'''
+
 
 def blockly_xml_to_tree(xml):
     pass
