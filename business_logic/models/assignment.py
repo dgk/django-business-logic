87c1c139bf9d8caa775d33580549225892e580c3
dgk <dgk@dgk.su>
2016-03-30 10:39:19 +0300

    Assignment..__str__

--- business_logic/models/assignment.py
+++ business_logic/models/assignment.py
@@ -6,11 +6,15 @@ from django.utils.translation import ugettext_lazy as _
 
 from node import NodeAccessor
 
+
 class Assignment(NodeAccessor):
 
     class Meta:
         verbose_name = _('Assignment')
         verbose_name_plural = _('Assignments')
+
+    def __str__(self):
+        return '='
 
     def interpret(self, ctx, lhs, rhs):
         lhs_node = ctx.get_children(self.node)[0]
