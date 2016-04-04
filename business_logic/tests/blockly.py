bd6d9ac6f3a77a948982d3730aaad5cad248167e
Dmitry Kuksinsky <dgk@dgk.su>
2016-04-04 09:44:01 +0300

    BlocklyXmlBuilderAssignmentTest

--- business_logic/tests/blockly.py
+++ business_logic/tests/blockly.py
@@ -33,14 +33,14 @@ class BlocklyXmlBuilderConstantTest(TestCase):
         self._constant_test(StringConstant(value='hello'), 'text', 'TEXT')
 
 
-class TreeToBlocklyXmlTest(TestCase):
+class BlocklyXmlBuilderAssignmentTest(TestCase):
     # https://blockly-demo.appspot.com/static/demos/code/index.html
 
 
     def test_assignment(self):
         entry_point = var_A_assign_1()
         assign_node = entry_point.get_children()[1]
-
+        print assign_node
         xml_str = tree_to_blockly_xml(assign_node)
         xml = etree.fromstring(xml_str)
-        #print(etree.tostring(xml, pretty_print=True))
+        print(etree.tostring(xml, pretty_print=True))
