259d672de159a6ce89f80c2c6d13ceb6d04ebcdf
Dmitry Kuksinsky <dgk@dgk.su>
2016-04-06 16:27:15 +0300

    tests.utils.var_A_assign_1() refactoring to variable_assign_value()

--- business_logic/tests/variable.py
+++ business_logic/tests/variable.py
@@ -6,7 +6,7 @@ from .common import *
 class VariableTest(TestCase):
     def test_definition(self):
         context = Context()
-        root = var_A_assign_1()
+        root = variable_assign_value()
         var_def = root.get_children()[0].content_object
         root.interpret(context)
         self.assertEqual(1, context.get_variable(var_def.id))
