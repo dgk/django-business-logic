345265d3721b2c9557d257dbd264465522f08a2b
Dmitry Kuksinsky <dgk@dgk.su>
2016-04-06 09:40:45 +0300

    validate_field_name error msg fix

--- business_logic/validators.py
+++ business_logic/validators.py
@@ -9,6 +9,6 @@ from django.utils.translation import ugettext_lazy as _
 field_name_re = re.compile(r'^[a-zA-Z][a-zA-Z0-9_.]+\Z')
 validate_field_name = RegexValidator(
     field_name_re,
-    _("Enter a valid 'field_name' consisting of letters, numbers, underscores."),
+    _("Enter a valid 'field_name' consisting of letters, numbers, underscores or dots."),
     'invalid'
 )
