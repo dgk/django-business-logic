6d97da3c7629f921d158b568a60c3504cc38ac28
dgk <dgk@dgk.su>
2016-04-06 16:20:16 +0300

    VariableTest fix

--- business_logic/tests/variable.py
+++ business_logic/tests/variable.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 #
 
-from django.test import TestCase
-from ..models import *
+from .common import *
 
 class VariableTest(TestCase):
     def test_definition(self):
         context = Context()
-        root = Node.add_root()
-        var_def = VariableDefinition(name='K1')
-        root.add_child(content_object=var_def)
-        var = Variable(definition=var_def)
-        var_node = root.add_child(content_object=var)
-        result = root.interpret(context)
+        root = var_A_assign_1()
+        var_def = root.get_children()[0].content_object
+        root.interpret(context)
+        self.assertEqual(1, context.get_variable(var_def.id))
 
