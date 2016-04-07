533200bb716914c5fd4e94430e23078ff9dd2ca1
dgk <dgk@dgk.su>
2016-04-07 17:53:48 +0300

    IfStatement dev snapshot

--- business_logic/models/ifstatement.py
+++ business_logic/models/ifstatement.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 #
 
-from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from .node import NodeAccessor
 
-class IfStatement(models.Model):
+
+class IfStatement(NodeAccessor):
+    interpret_children = True
+
     class Meta:
         verbose_name = _('If statement')
         verbose_name_plural = _('If statements')
 
+    def interpret(self, ctx):
+        children = ctx.get_children(self.node)
+        if_node = children[0]
+        if if_node.interpret(ctx):
+            children[1].interpret(ctx)
 
--- business_logic/tests/ifstatement.py
+++ business_logic/tests/ifstatement.py
@@ -6,21 +6,40 @@ from .common import *
 
 class IfStatementTest(TestCase):
     def test_interpret_if(self):
+        def reload_node(node):
+            return Node.objects.get(id=node.id)
+
         root = Node.add_root()
 
-        var_def1 = VariableDefinition(name='K1')
-        root.add_child(content_object=var_def1)
+        vars = ('IfCondition', 'IfEnter',)
+        var_defs = {}
+
+        for variable_name in vars:
+            var_def = VariableDefinition(name=variable_name)
+            var_defs[variable_name] = var_def
+            root.add_child(content_object=var_def)
+            root = reload_node(root)
+
+        ifstatement = root.add_child(content_object=IfStatement())
+        ifstatement.add_child(content_object=Variable(definition=var_defs['IfCondition']))
+
+        ifstatement = reload_node(ifstatement)
+
+        assignment = ifstatement.add_child(content_object=Assignment())
+
+        assignment.add_child(content_object=Variable(definition=var_defs['IfEnter']))
+        assignment.add_child(content_object=BooleanConstant(value=True))
+
         root = Node.objects.get(id=root.id)
 
-        var_def2 = VariableDefinition(name='K2')
-        root.add_child(content_object=var_def2)
-        root = Node.objects.get(id=root.id)
-
-        root.add_child(content_object=IfStatement())
-
-        for value, expected in ((True, 1), (False, 0)):
-            variable_assign_value()
-
-        integer_const = IntegerConstant(value=33)
         context = Context()
-        self.failUnlessEqual(33, integer_const.interpret(context))
+        root.interpret(context)
+        self.failIf(context.get_variable(var_defs['IfCondition'].id))
+        self.failIf(context.get_variable(var_defs['IfEnter'].id))
+
+
+        context = Context()
+        context.set_variable(var_defs['IfCondition'].id, True)
+        root.interpret(context)
+        self.failUnless(context.get_variable(var_defs['IfCondition'].id))
+        self.failUnless(context.get_variable(var_defs['IfEnter'].id))
