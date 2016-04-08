6934e4d09c9424a6f341b708b7e86442c61a73d6
Dmitry Kuksinsky <dgk@dgk.su>
2016-04-08 10:12:41 +0300

    IfStatementTest refactoring

--- business_logic/tests/ifstatement.py
+++ business_logic/tests/ifstatement.py
@@ -5,41 +5,45 @@ from .common import *
 
 
 class IfStatementTest(TestCase):
-    def test_interpret_if(self):
+    def setUp(self):
+        self.var_defs = {}
+
+    def create_tree(self, branches):
         def reload_node(node):
             return Node.objects.get(id=node.id)
 
         root = Node.add_root()
 
         vars = ('IfCondition', 'IfEnter',)
-        var_defs = {}
 
         for variable_name in vars:
             var_def = VariableDefinition(name=variable_name)
-            var_defs[variable_name] = var_def
+            self.var_defs[variable_name] = var_def
             root.add_child(content_object=var_def)
             root = reload_node(root)
 
         ifstatement = root.add_child(content_object=IfStatement())
-        ifstatement.add_child(content_object=Variable(definition=var_defs['IfCondition']))
+        ifstatement.add_child(content_object=Variable(definition=self.var_defs['IfCondition']))
 
         ifstatement = reload_node(ifstatement)
 
         assignment = ifstatement.add_child(content_object=Assignment())
 
-        assignment.add_child(content_object=Variable(definition=var_defs['IfEnter']))
+        assignment.add_child(content_object=Variable(definition=self.var_defs['IfEnter']))
         assignment.add_child(content_object=BooleanConstant(value=True))
 
-        root = Node.objects.get(id=root.id)
+        return reload_node(root)
 
+    def test_interpret_if(self):
+        root = self.create_tree(2)
         context = Context()
         root.interpret(context)
-        self.failIf(context.get_variable(var_defs['IfCondition'].id))
-        self.failIf(context.get_variable(var_defs['IfEnter'].id))
+        self.failIf(context.get_variable(self.var_defs['IfCondition'].id))
+        self.failIf(context.get_variable(self.var_defs['IfEnter'].id))
 
 
         context = Context()
-        context.set_variable(var_defs['IfCondition'].id, True)
+        context.set_variable(self.var_defs['IfCondition'].id, True)
         root.interpret(context)
-        self.failUnless(context.get_variable(var_defs['IfCondition'].id))
-        self.failUnless(context.get_variable(var_defs['IfEnter'].id))
+        self.failUnless(context.get_variable(self.var_defs['IfCondition'].id))
+        self.failUnless(context.get_variable(self.var_defs['IfEnter'].id))
