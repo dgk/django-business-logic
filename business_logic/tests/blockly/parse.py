bddce5303776f0325c46d1df30125f152b9082a6
dgk <dgk@dgk.su>
2016-04-12 18:03:33 +0300

    BlocklyXmlParser dev snapshot

--- business_logic/tests/blockly/parse.py
+++ business_logic/tests/blockly/parse.py
@@ -1,1 +1,19 @@
 # -*- coding: utf-8 -*-
+
+from django.utils.six import StringIO
+
+from lxml import etree
+
+from business_logic.tests.common import *
+
+
+class BlocklyXmlParserConstantTest(TestCase):
+    def test(self):
+        root = Node.add_root()
+        node = root.add_child(content_object=StringConstant(value='hello'))
+        print Node.dump_bulk(node)
+        xml_str = BlocklyXmlBuilder().build(node)
+
+        new_root = BlocklyXmlParser().parse(xml_str)
+        self.assertIsInstance(new_root, Node)
+
