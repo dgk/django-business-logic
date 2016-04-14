95c07474e1e41c9c697aeda23ed338f607d09651
dgk <dgk@dgk.su>
2016-04-14 17:38:02 +0300

    BlocklyXmlParser.visit_binary_operator

--- business_logic/blockly/data.py
+++ business_logic/blockly/data.py
@@ -23,3 +23,10 @@ OPERATOR_TABLE = {
 
 }
 
+REVERSE_OPERATOR_TABLE = {
+    block_type: {
+        blockly: internal for internal, blockly
+            in operators.items()}
+        for block_type, operators
+    in OPERATOR_TABLE.items()
+}
