21409a055b0282b595164b1f96aa709d9e17c037
Dmitry Kuksinsky <dgk@dgk.su>
2016-04-20 16:50:43 +0300

    NodeTreeCreator

--- business_logic/blockly/create.py
+++ business_logic/blockly/create.py
@@ -1,12 +1,31 @@
 # -*- coding: utf-8 -*-
 from django.contrib.contenttypes.models import ContentType
 
-from ..models import Variable, VariableDefinition
+from ..models import Variable, VariableDefinition, Node
 from ..utils import get_content_type_id
+
 
 class NodeTreeCreator(object):
     def create(self, data):
-        pass
+        def is_block(__data):
+            return 'content_type' not in __data['data']
+
+        variable_definitions = self.create_variable_definitions(data)
+        if not is_block(data):
+            data = {'data': {}, 'children':[data]}
+
+        data['children'] = variable_definitions + data['children']
+
+        def create_content_object(_data):
+            if not is_block(_data):
+                self.create_content_object(_data['data'])
+
+            for child in _data.get('children', []):
+                create_content_object(child)
+
+        create_content_object(data)
+
+        return Node.objects.get(id=Node.load_bulk([data, ])[0])
 
     def collect_objects(self, data, content_type_id):
         '''
@@ -19,7 +38,7 @@ class NodeTreeCreator(object):
         '''
         collection = []
         def collect(item):
-            if item['data']['content_type'] == content_type_id:
+            if item['data'].get('content_type') == content_type_id:
                 collection.append(item)
             children = item.get('children', [])
             for child in children:
@@ -30,8 +49,18 @@ class NodeTreeCreator(object):
     def create_content_object(self, data):
         content_type = ContentType.objects.get(id=data['content_type'])
         model_class = content_type.model_class()
-        object = model_class.objects.create()
+
+        if model_class == VariableDefinition:
+            return
+
+        node_kwargs = Node._meta.get_all_field_names()
+        content_object_kwargs = dict(((k, v) for k, v in data.items() if k not in node_kwargs))
+
+        object = model_class.objects.create(**content_object_kwargs)
         data['object_id'] = object.id
+        for kwarg in content_object_kwargs.keys():
+            del data[kwarg]
+
         return data
 
     def create_variable_definitions(self, data):
@@ -41,22 +70,22 @@ class NodeTreeCreator(object):
         for variable in variables:
             variable_name = variable['data']['name']
             if variable_name not in variable_by_name:
-                variable_definition_id = VariableDefinition.objects.create(name=variable_name)
+                variable_definition_id = VariableDefinition.objects.create(name=variable_name).id
                 variable_definition = {
                     'data': {
                         'content_type':  get_content_type_id(VariableDefinition),
-                        'content_object': variable_definition_id
+                        'object_id': variable_definition_id
                     }
                 }
                 variable_definitions.append(variable_definition)
                 variable_by_name[variable_name] = dict(variables=[],
-                           variable_definition=variable_definition_id)
+                        variable_definition=variable_definition_id)
             else:
                 variable_definition_id = variable_by_name[variable_name]['variable_definition']
 
             variable_by_name[variable_name]['variables'].append(variable)
             del variable['data']['name']
-            variable['data']['variable_definition'] = variable_definition_id
+            variable['data']['definition_id'] = variable_definition_id
 
         return variable_definitions
 
