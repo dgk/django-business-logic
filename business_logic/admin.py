4baf0c366ab724b447160920cde307cff5ce01ab
dgk <dgk@dgk.su>
2016-04-25 11:36:50 +0300

    ReferenceDescriptor.search_fields.null=True

--- business_logic/admin.py
+++ business_logic/admin.py
@@ -1,14 +1,9 @@
 # -*- coding: utf-8 -*-
-
 from django.contrib import admin
 
 from nested_inline.admin import NestedStackedInline, NestedModelAdmin
 
 from business_logic.models import ProgramType, ProgramArgument, ProgramArgumentField
-
-# register all app models for debug purposes
-# for model in apps.get_app_config('business_logic').get_models():
-#     admin.site.register(model)
 
 
 class ProgramArgumentFieldInline(NestedStackedInline):
@@ -31,3 +26,11 @@ class ProgramTypeAdmin(NestedModelAdmin):
 
 admin.site.register(ProgramType, ProgramTypeAdmin)
 
+
+# register all app models for debug purposes
+from django.apps import apps
+for model in apps.get_app_config('business_logic').get_models():
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
