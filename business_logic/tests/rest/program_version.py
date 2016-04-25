7c202337eb93184a3ad55fa9fd6676c18d515c35
dgk <dgk@dgk.su>
2016-04-25 18:33:53 +0300

    /rest/program-version: dev snapshot

--- business_logic/tests/rest/program_version.py
+++ business_logic/tests/rest/program_version.py
@@ -13,6 +13,18 @@ class ProgramVersionRESTTest(TestCase):
                                               name='test')
 
         self.program_version = ProgramVersion.objects.create(program=program, entry_point=variable_assign_value())
+        self.xml = BlocklyXmlBuilder().build(self.program_version.entry_point)
+
+    def test_program_version_create(self):
+        url = reverse('business-logic:rest:program-version-list')
+        from xml.sax.saxutils import escape
+        response = self.client.post(url,
+            dict(
+                program_version=self.program_version.id,
+                xml=escape(self.xml)
+            ))
+        self.assertEqual(201, response.status_code, response.content)
+
 
     def test_program_version_view(self):
         url = reverse('business-logic:rest:program-version', kwargs=dict(pk=self.program_version.id))
@@ -20,7 +32,5 @@ class ProgramVersionRESTTest(TestCase):
         self.assertEqual(200, response.status_code)
         _json = json.loads(response.content)
         self.assertIsInstance(_json, dict)
-        self.assertEqual(
-            BlocklyXmlBuilder().build(self.program_version.entry_point),
-            _json['xml'])
+        self.assertEqual(self.xml, _json['xml'])
 
