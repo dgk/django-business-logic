d400b626087b376b4027f15427769e3e89f1d01c
dgk <dgk@dgk.su>
2016-05-06 17:35:56 +0300

    Context.get_variable: recursive attribute seek

--- business_logic/tests/variable.py
+++ business_logic/tests/variable.py
@@ -3,6 +3,8 @@
 
 from .common import *
 # also see .assignment
+
+
 class VariableTest(TestCase):
     def test_definition(self):
         context = Context()
@@ -12,7 +14,6 @@ class VariableTest(TestCase):
         var = Variable(definition=var_def)
         var_node = root.add_child(content_object=var)
         result = root.interpret(context)
-
 
     def test_variable_undefined(self):
         undefined = Variable.Undefined()
@@ -25,3 +26,51 @@ class VariableTest(TestCase):
         root.add_child(content_object=var_def)
         root.interpret(context)
         self.assertIsInstance(context.get_variable(var_def), Variable.Undefined)
+
+    def test_get_variable_returns_recursive_attribute(self):
+        int_value = 8
+        string_value = 'hello'
+
+        test_model_variable_definition = VariableDefinition.objects.create(name='test_model')
+
+        test_model_foreign_value_string_value_variable_definition = VariableDefinition.objects.create(
+            name='test_model.foreign_value.string_value')
+
+        test_model_int_value_variable_definition = VariableDefinition.objects.create(name='test_model.int_value')
+
+        test_related_model = TestRelatedModel.objects.create(string_value=string_value)
+        test_model = TestModel.objects.create(int_value=int_value, foreign_value=test_related_model)
+
+        context = Context()
+        context.set_variable(test_model_variable_definition, test_model)
+
+        self.assertEqual(int_value, context.get_variable(test_model_int_value_variable_definition))
+        self.assertEqual(string_value, context.get_variable(test_model_foreign_value_string_value_variable_definition))
+
+    def test_get_variable_returns_recursive_attribute_undefined(self):
+        test_model_variable_definition = VariableDefinition.objects.create(name='test_model')
+
+        variable_definitions = {}
+
+        for field in (
+                'test_model.int_value',
+                'test_model.not_exists',
+                'test_model.foreign_value.string_value',
+                'test_model.foreign_value.not_exists',
+
+        ):
+            variable_definitions[field] = VariableDefinition.objects.create(name=field)
+
+        context = Context()
+
+        for variable_definition in variable_definitions.values():
+            self.assertIsInstance(context.get_variable(variable_definition), Variable.Undefined)
+
+        test_model = TestModel.objects.create()
+        context.set_variable(test_model_variable_definition, test_model)
+
+        for field, variable_definition in variable_definitions.items():
+            if field == 'test_model.int_value':
+                continue
+
+            self.assertIsInstance(context.get_variable(variable_definition), Variable.Undefined)
