460b0a98fb761f6b52730a73d0831e9fdb82d126
dgk <dgk@dgk.su>
2016-05-25 19:09:09 +0300

    setup.py: classifiers

--- setup.py
+++ setup.py
@@ -50,10 +50,12 @@ setup(
             "Intended Audience :: Developers",
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
-            "Programming Language :: Python",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3.4",
+            "Programming Language :: Python :: 3.5",
             "Framework :: Django",
         ],
         zip_safe=False,
         install_requires=[str(x.req)
-                          for x in parse_requirements(path('requirements.txt'))],
+                          for x in parse_requirements(path('requirements.txt'), session=False)],
 )
