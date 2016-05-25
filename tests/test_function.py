74771a2756b5c099dbfc83a553361c017181da12
dgk <dgk@dgk.su>
2016-05-25 18:37:06 +0300

    tests/test_function.py refactoring

--- tests/test_function.py
+++ tests/test_function.py
@@ -13,7 +13,7 @@ class FunctionTest(TestCase):
     def test_import(self):
         context = Context()
         root = Node.add_root()
-        func_def = FunctionDefinition(module='business_logic.tests.function', function='_bin')
+        func_def = FunctionDefinition(module=__name__, function='_bin')
         root.add_child(content_object=func_def)
         root = Node.objects.get(id=root.id)
         func = Function(definition=func_def)
@@ -26,7 +26,7 @@ class FunctionTest(TestCase):
     def test_context_in_function(self):
         context = Context()
         root = Node.add_root()
-        func_def = FunctionDefinition(module='tests.test_function',
+        func_def = FunctionDefinition(module=__name__,
                 function='context_bin', context_required=True)
         root.add_child(content_object=func_def)
         root = Node.objects.get(id=root.id)
