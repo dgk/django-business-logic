84ea7b82d1c194041e710482262659b5599e4251
dgk <dgk@dgk.su>
2016-06-01 17:36:47 +0300

    sites.dev: redirect to /books/book instead 404 err

--- sites/dev/urls.py
+++ sites/dev/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.shortcuts import redirect
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'demo.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url('^admin/', include(admin.site.urls)),
     url('^business-logic/', include('business_logic.urls')),
     url('^books/', include('sites.dev.books.urls')),
+    url('.*', lambda x: redirect('/books/book')),
 ]
