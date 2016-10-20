f305bf9ee825deb58602d5dbfe8003d5dde2c155
MrLion9 <finthehuman9@gmail.com>
2016-10-20 16:48:33 +0300

    forgot file

--- frontend/src/app/version/version.component.ts
+++ frontend/src/app/version/version.component.ts
@@ -1,10 +1,13 @@
 import { Component, NgModule } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { BackendService } from '../backend.service';
+import { BreadcrumbComponent } from '../breadcrumb/breadcrumb.component';
 
 @Component({
   selector: 'version',
-  template: `<md-list>
+  template: `
+            <breadcrumb [params]="params"></breadcrumb>
+            <md-list>
                 <md-list-item *ngFor="let version of versions" (click)="onSelect(version.id)">
                   <h3 md-line>{{version.title}}</h3>
                 </md-list-item>
@@ -13,6 +16,7 @@ import { BackendService } from '../backend.service';
 
 export class VersionComponent{
   private versions;
+  private params: any = {};
 
   constructor(
     public backend: BackendService,
@@ -23,6 +27,8 @@ export class VersionComponent{
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
+      this.params = params;
+
       this.backend.listProgramVersions(+params['programID']).subscribe((envelope) => {
         this.versions = envelope.results;
       });
