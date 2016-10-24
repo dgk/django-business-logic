865995040de825e4f0708c7debac871c2f6f8c70
MrLion9 <finthehuman9@gmail.com>
2016-10-24 15:42:39 +0300

    technical commit

--- frontend/src/app/home/home.component.spec.ts
+++ frontend/src/app/home/home.component.spec.ts
@@ -3,7 +3,7 @@ import {
   TestBed,
   ComponentFixture
 } from '@angular/core/testing';
-import { Component } from '@angular/core';
+import { Component, DebugElement } from '@angular/core';
 import {
   BaseRequestOptions,
   ConnectionBackend,
@@ -14,31 +14,50 @@ import { MockBackend } from '@angular/http/testing';
 // Load the implementations that should be tested
 import { AppState } from '../app.service';
 import { HomeComponent } from './home.component';
+import { BreadcrumbComponent } from '../breadcrumb/breadcrumb.component';
 
+let fixture: ComponentFixture<HomeComponent>;
+let comp:    HomeComponent;
+let de:      DebugElement;
+let el:      HTMLElement;
 
-describe('Home', () => {
+describe('HomeComponent', () => {
   // provide our implementations or mocks to the dependency injector
   beforeEach(() => {
-      TestBed.configureTestingModule({
-        providers: [
-          BaseRequestOptions,
-          MockBackend,
-          {
-            provide: Http,
-            useFactory: function(backend: ConnectionBackend, defaultOptions: BaseRequestOptions) {
-              return new Http(backend, defaultOptions);
-            },
-            deps: [MockBackend, BaseRequestOptions]
+    TestBed.configureTestingModule({
+      declarations: [ HomeComponent, BreadcrumbComponent ],
+      providers: [
+        BaseRequestOptions,
+        MockBackend,
+        {
+          provide: Http,
+          useFactory: function(backend: ConnectionBackend, defaultOptions: BaseRequestOptions) {
+            return new Http(backend, defaultOptions);
           },
-          AppState,
-          HomeComponent
-        ]
-      });
+          deps: [MockBackend, BaseRequestOptions]
+        },
+        AppState,
+        HomeComponent,
+        BreadcrumbComponent
+      ]
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(HomeComponent);
+      comp = fixture.componentInstance;
+      de = fixture.debugElement.query(By.css('h3'));
+      el = de.nativeElement;
+    });
   });
 
-  it('hello honey, iam home!', inject( [HomeComponent], (home: HomeComponent)=>{
-    expect(1+1).toEqual(2);
-  } ));
+
+  // it('hello honey, iam home!', inject( ()=>{
+  //   expect(1+1).toEqual(2);
+  // } ));
+
+  it('should display original href', () => {
+    //console.log(comp);
+    fixture.detectChanges();
+    // expect(el.textContent).toContain(comp.href);
+  });
 
   // it('should have default data', inject([ HomeComponent ], (home: HomeComponent) => {
   //   expect(home.localState).toEqual({ value: '' });
