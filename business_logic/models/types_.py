2c64b487f137b12c755ec7197c5c05824ebf6eed
dgk <dgk@dgk.su>
2016-10-27 11:20:44 +0300

    rm obsolete models

--- business_logic/models/types_.py
+++ business_logic/models/types_.py
@@ -3,11 +3,9 @@
 
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django.contrib.contenttypes.models import ContentType
 
 from .constant import BooleanConstant
 from .constant import NumberConstant
-from .constant import ModelConstant
 from .constant import StringConstant
 
 CONSTANTS_FOR_TYPES = {
@@ -52,18 +50,3 @@ for _type, fields in DJANGO_FIELDS_FOR_TYPES.items():
     for field in fields:
         TYPES_FOR_DJANGO_FIELDS[field] = _type
 
-
-class Type(models.Model):
-    type = models.CharField(_('Type'), max_length=12, choices=TYPE_CHOICES, blank=False, null=False)
-    model_type = models.ForeignKey(ContentType, verbose_name=_('Model type'), null=True)
-    is_list = models.BooleanField(_('Is list'))
-
-    class Meta:
-        verbose_name = _('Type')
-        verbose_name_plural = _('Types')
-
-    @classmethod
-    def get_constant_for_type(cls, type):
-        if type.type == 'model':
-            return type.model_type.model_class()
-        return CONSTANTS_FOR_TYPES[type.type]
