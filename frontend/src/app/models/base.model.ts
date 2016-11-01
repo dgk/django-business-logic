2530b76d67f8319f8838bd65c45899f74ed54430
MrLion9 <finthehuman9@gmail.com>
2016-11-01 18:01:13 +0300

    forgot file

--- frontend/src/app/models/base.model.ts
+++ frontend/src/app/models/base.model.ts
@@ -1,5 +1,26 @@
-export class BaseModel{
+import { BaseCollection } from "./base.collection";
+
+export abstract class BaseModel{
+  baseUrl: string = '/business-logic/rest';
+
   id: number;
   url: string;
   title: string;
+
+  constructor(id: number, title: string){
+    this.id = id;
+    this.title = title;
+  }
+
+  getID(): number{
+    return this.id;
+  }
+
+  getUrl(): string{
+    return this.baseUrl + this.url;
+  }
+
+  getTitle(): string{
+    return this.title;
+  }
 }
