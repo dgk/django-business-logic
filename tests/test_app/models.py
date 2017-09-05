9e0cd391f05ccb44360e3796572056ec0a15f3dc
dgk <dgk@dgk.su>
2017-09-06 01:50:59 +0300

    django 1.11 support

--- tests/test_app/models.py
+++ tests/test_app/models.py
@@ -8,7 +8,7 @@ from django.db import models
 
 class TestModel(models.Model):
     int_value = models.PositiveIntegerField('Integer value', default=1)
-    decimal_value = models.DecimalField('Decimal value', default=Decimal('2.0'))
+    decimal_value = models.DecimalField('Decimal value', max_digits=10, decimal_places=4, default=Decimal('2.0'))
     string_value = models.CharField(max_length=255)
     date_value = models.DateField(null=True, blank=True)
     datetime_value = models.DateTimeField(null=True, blank=True)
