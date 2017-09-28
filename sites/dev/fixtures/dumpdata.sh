ddd976a47fddf97fb30bfed8604d1200b0f6be2a
dgk <dgk@dgk.su>
2017-09-29 01:49:15 +0300

    sites/dev/fixtures/data.json: sample data added

--- sites/dev/fixtures/dumpdata.sh
+++ sites/dev/fixtures/dumpdata.sh
@@ -1,1 +1,5 @@
-python manage.py dumpdata --natural-foreign --indent 4 -e sessions -e contenttypes -e admin.logentry -e auth.permission > sites/dev/fixtures/data.json
+python manage.py dumpdata --natural-foreign --indent 4 \
+-e sessions -e contenttypes -e admin.logentry -e auth.permission \
+-e business_logic.logentry -e business_logic.execution -e business_logic.executionargument \
+> sites/dev/fixtures/data.json
+
