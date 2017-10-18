ba667d3fc8f0e8456ca7030c62b7481400554d89
vlfedotov <fedotov@gmail.com>
2017-10-18 08:19:40 +0300

    divide dev-test-base settings

--- setup.py
+++ setup.py
@@ -55,7 +55,6 @@ def get_package_data(package):
 def path(*parts):
     return os.path.join(os.path.dirname(__file__), *parts)
 
-
 def clean():
     for d in ('dist', 'build', '{}.egg-info'.format(NAME.replace('-', '_'))):
         if os.path.exists(d):
@@ -81,77 +80,51 @@ if sys.argv[-1] == 'publish':
 
 
 setup(
-    name=NAME,
-    version=get_version(PACKAGE),
-    description=DESCRIPTION,
-    long_description=codecs.open(path('README.rst'), encoding='utf-8').read(),
-    author=AUTHOR,
-    author_email=AUTHOR_EMAIL,
-    url=URL,
-    #download_url='{}/archive/{}.tar.gz'.format(URL, VERSION),
-    packages=get_packages(PACKAGE),
-    package_data={
-        PACKAGE: [
-            'locale/*/LC_MESSAGES/django.[mp]o',
-            'static/business_logic/*.html',
-            'static/business_logic/*.js',
-            'static/business_logic/*.map',
-            'static/business_logic/blockly/*',
-            'static/business_logic/src/assets/fonts/*',
-        ]
-    },
-    license='MIT',
-    keywords=['django', ],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Framework :: Django',
-        'Framework :: Django :: 1.8',
-        'Framework :: Django :: 1.9',
-        'Framework :: Django :: 1.10',
-        'Framework :: Django :: 1.11',
-    ],
-    zip_safe=False,
-    scripts=['manage.py'],
-    install_requires=[
-        #'pyparsing>=2.0.3',
-        'django-treebeard==4.1.2',
-        'djangorestframework<3.7',
-        'django-filter==1.0.4',
-        'Markdown<3.0',
-        'django-nested-inline==0.3.7',
-        'lxml<4.0',
-        'django-polymorphic==1.3.0',
-        'django-ace-overlay==0.6',
-        'django-bootstrap3==9.0.0',
-        'django-admin-sortable2==0.6.15',
-    ],
-    extras_require={
-        'test': [
-            'pip>=9.0.1',
-            'pytest>=3.2.1',
-            'pytest-cov>=2.5.1',
-            'pytest-django>=3.1.2',
-            'colorama>=0.3.9',
-            'coverage>=4.4.1',
-            'django-nose>=1.4.5',
-            'nose>=1.3.7',
-            'pinocchio>=0.4.2', 
+        name=NAME,
+        version=get_version(PACKAGE),
+        description=DESCRIPTION,
+        long_description=codecs.open(path('README.rst'), encoding='utf-8').read(),
+        author=AUTHOR,
+        author_email=AUTHOR_EMAIL,
+        url=URL,
+        #download_url='{}/archive/{}.tar.gz'.format(URL, VERSION),
+        packages=get_packages(PACKAGE),
+        package_data={
+            PACKAGE: [
+                'locale/*/LC_MESSAGES/django.[mp]o',
+                'static/business_logic/*.html',
+                'static/business_logic/*.js',
+                'static/business_logic/*.map',
+                'static/business_logic/blockly/*',
+                'static/business_logic/src/assets/fonts/*',
+            ]
+        },
+
+        license='MIT',
+        keywords=['django', ],
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Environment :: Web Environment',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Topic :: Internet :: WWW/HTTP',
+            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+            'Framework :: Django',
+            'Framework :: Django :: 1.8',
+            'Framework :: Django :: 1.9',
+            'Framework :: Django :: 1.10',
+            'Framework :: Django :: 1.11',
         ],
-        'dev': [
-            'Django>=1.11.5',
-            'tox>=2.8.1',
-        ],
-    },
+        zip_safe=False,
+        install_requires=[
+            str(x.req) for x
+                in parse_requirements(path('requirements.txt'), session=False)
+            ],
 )
