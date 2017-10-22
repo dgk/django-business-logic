9fc40e5932476556035b6e9be6118ad41e783ad4
dgk <dgk@dgk.su>
2017-10-22 21:15:54 +0300

    pep8: E129 visually indented line with same indent as next logical line

--- business_logic/models/node.py
+++ business_logic/models/node.py
@@ -43,7 +43,7 @@ class Node(NS_Node):
 
     def delete(self):
         if (self.object_id and self.content_object and
-            self.content_type.app_label == ContentType.objects.get_for_model(self.__class__).app_label):
+                self.content_type.app_label == ContentType.objects.get_for_model(self.__class__).app_label):
             self.content_object.delete()
 
         for child in self.get_children():
