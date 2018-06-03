36403c9d79acf98064992936b9e254c7861dbad9
dgk <dgk@dgk.su>
2018-06-03 18:23:01 +0300

    fix for pip>=10

--- setup.py
+++ setup.py
@@ -7,7 +7,10 @@ import sys
 import shutil
 from setuptools import setup, find_packages
 
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 PACKAGE = "business_logic"
 NAME = "django-business-logic"
