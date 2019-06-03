49cdb7c0843b163bd017c6451a1b0156f9672215
Marc Queiroz <marc.queiroz@gmail.com>
2019-06-03 15:17:03 -0300

    Fix for Whiteghost 4.0

--- sites/settings.py
+++ sites/settings.py
@@ -38,6 +38,7 @@ MIDDLEWARE = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 )
 
 TEMPLATES = [
