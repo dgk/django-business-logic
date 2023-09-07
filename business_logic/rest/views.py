3223e16d29b7c6b00320641f192646be538d4c71
andrejromanov <andrej.romanov.webdev@gmail.com>
2023-09-07 23:50:22 +0300

    get rid of unused attribute

--- business_logic/rest/views.py
+++ business_logic/rest/views.py
@@ -51,7 +51,6 @@ class StandardResultsSetPagination(PageNumberPagination):
     page_size = 50
     page_size_query_param = 'page_size'
     max_page_size = 1000
-    ordering = "id"
 
 
 class ObjectList(generics.ListAPIView):
